3d3b3705ef66111ba0a9f3711a0167032667dc69
Sebastian Higuita <sebastianhc1995@hotmail.com>
2019-03-10 22:18:14 -0500

    Create basic sample of ODI framework

---
+++ src/services/history.service.ts
@@ -0,0 +1,21 @@
+import { Service } from "odi";
+
+export interface Message {
+  username: string;
+  text: string;
+}
+
+@Service()
+export default class HistoryService {
+  private store: Message[] = [];
+
+  getMessages() {
+    return this.store;
+  }
+
+  addMessage(message: Message) {
+    if (this.store.length > 10) this.store.pop();
+
+    this.store.push(message);
+  }
+}
